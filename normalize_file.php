9ebbc024f711a9d93ab82ca7cb9c2d1ede42374f
David Carvalho <davidcarvalho.developer@gmail.com>
2021-07-01 13:45:09 -0400

    Adicionado autor.

--- normalize_file.php
+++ normalize_file.php
@@ -9,6 +9,8 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 /**
+* @author David Carvalho <davidcarvalho.developer@gmail.com>
+*
 * Script utilizado em um projeto particular para normalizar os nomes 
 * dos arquivos e atualizar sua referência no banco de dados.
 *
